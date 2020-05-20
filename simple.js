e46c5f3a70da73e2240f02146622c531dca3cfae
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-05-20 05:26:02 -0700

    first solutions

--- simple.js
+++ simple.js
@@ -3,7 +3,43 @@
  function footballPoints (win, draw, losses){
      var x = win * 3;
      var y = draw * 1;
-     var z = loss * 0;
+     var z = losses * 0;
      return x + y + z
  }
- 
+ /** create a function that finds the perimeter of a rectangle using width and height **/
+  function perimeterRectangle (width, height){
+      var x = width * 2;
+      var y = height * 2;
+      return x + y
+  }
+
+  /** Create a function that takes an array and returns the first element. **/
+  function firstInArray (arr){
+var arr = [1, 2, 3];
+return arr[0]
+  }
+  // function that adds two numbers
+  function addition(a, b) {
+	const y = a;
+	const x =b;
+	return y + x
+}
+// a function that increments a number by 1
+function addition(num) {
+    let a = num;
+    return a+=1;
+}
+//function to convert minutes to seconds
+function convert(minutes) {
+    var h = minutes;
+    var seconds = 60;
+    return h * seconds;
+}
+// function to return true or false if num <= 0
+function lessThanOrEqualToZero(num) {
+	if (num <= 0){
+        return true
+    }else{
+        return false
+    }
+}
