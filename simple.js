4112999ba8644ce2cea582786ee5063177196ac3
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-05-20 04:25:43 -0700

    initial commit

---
+++ simple.js
@@ -0,0 +1,9 @@
+/** create a function that takes the number of wins, draw and losses 
+ and calculate the number of points a football team has obtained  A win receives 3 points, a draw 1 point and a loss 0 points. **/
+ function footballPoints (win, draw, losses){
+     var x = win * 3;
+     var y = draw * 1;
+     var z = loss * 0;
+     return x + y + z
+ }
+ 
